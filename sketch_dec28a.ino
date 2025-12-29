6cf6ee3e6ada6403d05ca65d7a34a56cd5be88e4
vsaucemichael666-alt <vsaucemichael666@gmail.com>
2025-12-29 16:00:52 +0500

    Initial commit

---
+++ sketch_dec28a.ino
@@ -0,0 +1,43 @@
+int trig = 3;
+int echo = 5;
+int buzzer = 6;
+void setup()
+{
+pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
+pinMode(buzzer, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+digitalWrite(trig, LOW);
+delayMicroseconds(10);
+digitalWrite(trig, HIGH);
+delayMicroseconds(10);
+digitalWrite(trig, LOW);
+
+int time = pulseIn(echo, HIGH);
+int dist = time * 0.034 / 2;
+delay(100);
+
+  if (dist >= 20) {
+    tone(buzzer, 100);
+  }
+  if (dist < 20 && dist >= 10) {
+    tone(buzzer, 500);
+    delay(100);
+    noTone(buzzer);
+    delay(100);
+  }
+  if (dist < 10) {
+    tone(buzzer, 1000);
+    delay(50);
+    noTone(buzzer);
+    delay(50);
+ 
+  }
+  if (dist > 25) {
+    noTone(buzzer);
+  }
+}
