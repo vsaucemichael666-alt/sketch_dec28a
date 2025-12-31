8153469a054d4bed554c339db36905f827a4edff
vsaucemichael666-alt <vsaucemichael666@gmail.com>
2025-12-31 13:50:45 +0500

    Update sketch_dec28a.ino

--- sketch_dec28a.ino
+++ sketch_dec28a.ino
@@ -15,28 +15,37 @@ delayMicroseconds(10);
 digitalWrite(trig, HIGH);
 delayMicroseconds(10);
 digitalWrite(trig, LOW);
-
+  
 int time = pulseIn(echo, HIGH);
 int dist = time * 0.034 / 2;
 delay(100);
-
-  if (dist >= 20) {
-    tone(buzzer, 100);
+  if (dist > 300) {
+    noTone(buzzer);
   }
-  else if (dist < 20 && dist >= 10) {
+  else if (dist < 300 && dist >= 200) {
     tone(buzzer, 500);
     delay(100);
     noTone(buzzer);
     delay(100);
   }
-  else if (dist < 10) {
+  else if (dist < 200 && dist >= 100) {
     tone(buzzer, 1000);
-    delay(50);
+    delay(100);
     noTone(buzzer);
-    delay(50);
+    delay(100);
  
   }
-  else if (dist > 25) {
+  else if (dist < 100 && dist >= 50) {
+    tone(buzzer, 1500);
+    delay(100);
     noTone(buzzer);
+    delay(100);
+  
+}
+  else if (dist < 50) {
+    tone(buzzer, 2000);
+    delay(100);
+    noTone(buzzer);
+    delay(100);
   }
 }
